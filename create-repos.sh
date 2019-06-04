03cf2ba750cf44eccc041184361d1f7c1cb26bd2
Kelly Keating <keating.kel@gmail.com>
2019-06-05 10:41:36 +1200

    script works but needs improving

---
+++ create-repos.sh
@@ -0,0 +1,104 @@
+#!/bin/sh
+# This is a comment!
+
+
+
+# Enter your username here (will then request password)
+GITHUB_USER="kelly-keating"
+
+COHORT="kotare-2019"
+
+# Repos you want to clone (make sure you match the name on eda-challenges)
+REPOS=("enspiraled")
+
+
+EDA_ORG="https://github.com/dev-academy-challenges"
+STUDENT_ORG="https://github.com/$COHORT"
+
+
+
+function clone_repos {
+
+    echo "Starting script\n"
+
+    for repo in "${REPOS[@]}"; do
+        clone_repo
+        make_new_repo 
+        push_repo
+        protect_master
+    done
+
+    echo "\nDone :)"
+}
+
+
+#### COPY EXISTING REPO
+
+function clone_repo {
+
+    echo "Copying repo $repo - $EDA_ORG/$repo.git\n"
+
+    # if remote exists, otherwise
+    ( git ls-remote $EDA_ORG/$repo.git -q ) && ( git clone $EDA_ORG/$repo.git ) || ("$repo  -  No such repo $EDA_ORG/$repo.git")
+}
+
+
+#### MAKE NEW REPO ON STUDENT GITHUB
+
+function make_new_repo {
+
+    echo "\nCreating repo $repo on Github\n"
+
+    # TO ADD: "description": "This is your repository description",
+    new_repo_data='{"name":"'"$repo"'"}'
+
+    # curl at github api
+    ( curl --user $GITHUB_USER -X POST --data "$new_repo_data" https://api.github.com/orgs/$COHORT/repos > /dev/null ) && ( echo "\nSuccessfully created repo $repo" )
+}
+
+
+
+#### PUSH A FULL COPY OF REPO
+
+function push_repo {
+
+    cd $repo
+
+    allBranches=$(git branch -r)
+    allBranches=${allBranches#*origin/HEAD -> origin/master}
+
+    for branch in $allBranches; do
+        push_branch ${branch#*origin/}
+    done
+
+    cd ..
+    rm -rf $repo
+}
+
+
+function push_branch {
+
+    branch=$1
+    echo "\nCopying branch $branch"
+
+    git checkout $branch
+    git push $STUDENT_ORG/$repo.git $branch
+}
+
+
+#### PROTECT MASTER
+
+function protect_master {
+
+    echo "\nUpdating master permissions\n"
+
+    # This is the branch permissions so that only admin can push to that branch
+    permission_settings='{"required_status_checks":null,"enforce_admins":null,"required_pull_request_reviews":null,"restrictions":{"users":[],"teams":[]}}'
+
+    # Parse it into json
+    permissions=$(jq <<< $permission_settings)
+
+    ( curl --user $GITHUB_USER -X PUT --data "$permissions" https://api.github.com/repos/$COHORT/$repo/branches/master/protection > /dev/null ) && ( echo "\nSuccessfully updated master permissions on $repo" )
+}
+
+clone_repos
